2d0e3d8123cb70709f7ba565cda12fab0d1fd447
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-04-01 22:00:07 -0300

    test: test item classes

---
+++ game/items/test.js
@@ -0,0 +1,9 @@
+const { Items, Food, FirstAid, Survival } = require('./items')
+
+const food = new Food()
+const firstaid = new FirstAid()
+const survival = new Survival()
+
+console.log(food)
+console.log(firstaid)
+console.log(survival)
