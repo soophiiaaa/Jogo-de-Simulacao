c5812f572345dedfaf285dd00aed242bdc1ba2ce
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-04-17 19:31:10 -0300

    feat: add function chooseItems()

--- game/functions/choose.js
+++ game/functions/choose.js
@@ -1,10 +1,34 @@
-const prompt = require('prompt-sync')
-
-const Character = require('../character/character') 
+const Character = require('../models/character')
 const { Items, Food, FirstAid, Survival } = require('../items/items')
 
 let character = new Character
+let items = new Items
+let food = new Food
+let firstaid = new FirstAid
+let survival = new Survival
 
 function chooseItems(character) {
-    let choose = parseInt(prompt('Escolha seus itens dentro de 60 segundos: '))
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    }) //lê e exibe o que o usuário digitou
+
+    let timer
+
+    readline.question('Escolha o número dos seus itens: ', (input) => {
+
+        
+
+        character.resources.push(input)
+        clearTimeout(timer)
+        readline.close()
+    })
+
+    setTimeout(() => {
+        console.log('⏰ O tempo acaba e você corre para um local seguro...')
+        console.log(character.resources)
+        readline.close()
+    }, 6000)
 }
+
+chooseItems(character)
