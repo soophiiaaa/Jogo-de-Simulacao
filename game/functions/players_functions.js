c543cb8a57850632e640431d1acb1127a8e3929e
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-04-18 20:08:05 -0300

    feat: add functions for json file

---
+++ game/functions/players_functions.js
@@ -0,0 +1,50 @@
+const prompt = require('prompt-sync')()
+const fs = require('fs')
+const path = require('path')
+const Character = require('../models/character')
+
+function readPlayers() {
+    const filePath = path.join(__dirname, '../data/players.json')
+    const data = fs.readFileSync(filePath, 'utf-8')
+    const players = JSON.parse(data)
+    return players
+} //lê os jogadores salvos
+
+function savePlayers(character) {
+    const filePath = path.join(__dirname, '../data/players.json')
+    const players = readPlayers()
+
+    players.push(character)
+
+    fs.writeFileSync(filePath, JSON.stringify(players, null, 2), 'utf-8')
+} //salva o jogador atual
+
+function newGame() {
+    let name = prompt('Digite seu nome: ')
+    let character = new Character
+    character.name = name
+    savePlayers(character)
+    console.log(`Bem-vindo, ${name}! Seu novo jogo começou!`)
+} //inicia um novo jogo
+
+function loadGame(callback) {
+    const players = callback
+
+    let name = prompt('Qual o seu nome, sobrevivente? ')
+    let found = true;
+
+    for (i = 0; i < players.length; i++) {
+        if (players[i].name === name) {
+            console.log(`Bem-vindo de volta, ${player.name}!`)
+            found = true
+            break
+        }
+    }
+
+    if (!found) {
+        console.log('Jogador não encontrado. Vamos começar um novo jogo!');
+        newGame()
+    }
+}
+
+module.exports = { readPlayers, newGame, loadGame }
