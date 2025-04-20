db7278b6304906044f869f89523bb4d9ac03f948
Pedro Henrique <166749723+pedropaivaa@users.noreply.github.com>
2025-04-19 21:56:52 -0300

    testEvent.js

--- game/event/test_event.js
+++ game/event/test_event.js
@@ -1,24 +1,12 @@
 const {
-<<<<<<< HEAD:game/event/testeEvent.js
-   Catastrofe,
-   Intergalactica,
-   InvasaoAlienigena,
-   ColapsoIA,
-   RatosMutantes,
-   Terremoto
- } = require ("./eventos")
-=======
-    Catastrofe,
-    Intergalactica,
-    InvasaoAlienigina,
-    ColapsoIA,
-    RatosMutantes,
-    Terremoto,
-    desastres
- } = require ("./event")
->>>>>>> c543cb8a57850632e640431d1acb1127a8e3929e:game/event/test_event.js
+Catastrofe,
+Intergalactica,
+InvasaoAlienigena,
+ColapsoIA,
+RatosMutantes,
+Terremoto
+} = require ("./event")
 
-//  const catastrofe = new Terremoto(); // erro: pq o static pertence a classe  nao aINSTANCIA
-const catastrofe = Catastrofe.gerarDesastreAleatorio() 
-catastrofe.introducao();
+const catastrofe = Catastrofe.gerarDesastreAleatorio()
+catastrofe.introducao()
  
